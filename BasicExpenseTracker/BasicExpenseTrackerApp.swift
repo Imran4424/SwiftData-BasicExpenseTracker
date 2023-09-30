443cd2584788f49fa2569cb3555ce73eeef3ad92
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-09-30 20:19:39 +0600

    Inject model in global space

--- BasicExpenseTracker/BasicExpenseTrackerApp.swift
+++ BasicExpenseTracker/BasicExpenseTrackerApp.swift
@@ -6,12 +6,23 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct BasicExpenseTrackerApp: App {
+  // use this custom container if you need customization
+//    let container: ModelContainer = {
+//        let schema = Schema([Expense.self])
+//        let container = try! ModelContainer(for: schema, configurations: [])
+//        return container
+//    }()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+//        .modelContainer(container)
+        .modelContainer(for: [Expense.self])
+        // basic way, when no customization needed in future
     }
 }
