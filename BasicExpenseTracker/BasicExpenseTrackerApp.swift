93b32000c44ae25abd301e870fba4d94a7d1431d
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-09-30 19:14:07 +0600

    project init

---
+++ BasicExpenseTracker/BasicExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicExpenseTrackerApp.swift
+//  BasicExpenseTracker
+//
+//  Created by Shah Md Imran Hossain on 30/9/23.
+//
+
+import SwiftUI
+
+@main
+struct BasicExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
